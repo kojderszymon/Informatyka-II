fe66bf90d36f2e1b48dc1524fcf4847306d957c5
kojderszymon <vaser1999@gmail.com>
2020-10-17 22:05:40 +0200

    Zadanie 2

--- LAB1/Zadanie 2/zadanie_2.cpp
+++ LAB1/Zadanie 2/zadanie_2.cpp
@@ -8,14 +8,14 @@ int main(int argc, char *argv[]){
     bool is_palindrome = 1;
 
     for(int i = 0; i < letters/2; ++i)
-        if(palindrome[i] != palindrome[letters - i]){
+        if(palindrome[i] != palindrome[letters - i - 1]){
             is_palindrome = 0;
             break;
         }
     
     if(is_palindrome == 1)
         cout << "Podany wyraz jest palindromem" << endl;
-    else
+    else if(is_palindrome == 0)
         cout << "Podany wyraz nie jest palindromem" << endl;
 
     return 0;
