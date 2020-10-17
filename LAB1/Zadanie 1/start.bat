29c8af65e0863bcae429284761f164e1b297b145
kojderszymon <vaser1999@gmail.com>
2020-10-17 21:19:16 +0200

    ok

---
+++ LAB1/Zadanie 1/start.bat
@@ -0,0 +1,4 @@
+@echo off
+g++ zadanie_1.cpp -o zadanie_1.exe
+zadanie_1.exe 100 102 130 105 108 134 156 178 190
+pause
--- LAB1/start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-g++ sort.cpp -o sort.exe
-sort.exe 100 102 130 105 108 134 156 178 190
-pause
