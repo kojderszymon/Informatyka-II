8bb42ac676846eed4d8e5db795aaf513f8016d73
kojderszymon <vaser1999@gmail.com>
2020-10-17 21:38:44 +0200

    Zadanie 2

--- LAB1/Zadanie 2/start.bat
+++ LAB1/Zadanie 2/start.bat
@@ -1,4 +1,4 @@
 @echo off
 g++ zadanie_2.cpp -o zadanie_2.exe
-zadanie_2.exe 100 102 130 105 108 134 156 178 190
+zadanie_2.exe kajak
 pause
--- LAB1/Zadanie 2/zadanie_2.cpp
+++ LAB1/Zadanie 2/zadanie_2.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+int main(int argc, char *argv[]){
+
+    string palindrome = argv[1];
+    int letters = palindrome.size();
+
+    for(int i = 0; i < letters/2; ++i){
+        if(palindrome[i] != palindrome[letters - i]){
+            cout << "Wyraz nie jest palindromem" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
