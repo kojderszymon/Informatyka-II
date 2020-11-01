796908d102db9fd6eb057d1510099af79f38e930
kojderszymon <vaser1999@gmail.com>
2020-11-01 16:04:50 +0100

    Zadanie6

--- LAB2/Zadanie 6/zadanie_6.cpp
+++ LAB2/Zadanie 6/zadanie_6.cpp
@@ -15,15 +15,15 @@ int main(int argc, char* argv[]) {
     *number2 = 45;
 
     cout << "Przed zamiana:" << endl;
-    cout << "Liczba 1: " << *number1 << endl;
-    cout << "Liczba 2: " << *number2 << endl;
+    cout << "Liczba 1: " << number1 << " " << *number1 <<  endl;
+    cout << "Liczba 2: " << number2 << " " << *number2 <<  endl;
 
     //zamiana wskaznikow
     my_swap(&number1, &number2);
 
     cout << "Po zamianie:" << endl;
-    cout << "Liczba 1: " << *number1 << endl;
-    cout << "Liczba 2: " << *number2 << endl;
+    cout << "Liczba 1: " << number1 << " " << *number1 <<  endl;
+    cout << "Liczba 2: " << number2 << " " << *number2 <<  endl;
 
     return 0;
 }
