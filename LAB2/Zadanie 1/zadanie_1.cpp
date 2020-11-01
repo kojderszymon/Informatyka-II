427eed6b32951f2d60c01827e297f5b329f29ddd
kojderszymon <vaser1999@gmail.com>
2020-11-01 14:17:54 +0100

    Zadanie2

--- LAB2/Zadanie 1/zadanie_1.cpp
+++ LAB2/Zadanie 1/zadanie_1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void my_swap(int * number1, int * number2) {
+void my_swap(int *number1, int *number2) {
     if(*number1 > *number2) {
         int temp = *number1;
         *number1 = *number2;
@@ -12,21 +12,25 @@ void my_swap(int * number1, int * number2) {
 int main(int argc, char *argv[]) {
 
     int number1 = 13, number2 = 54;
+
     cout << "Przed zamiana:" << endl;
     cout << "Liczba 1: " << number1 << endl;
     cout << "Liczba 2: " << number2 << endl;
 
     my_swap(&number1, &number2);
+
     cout << "Po zamianie:" << endl;
     cout << "Liczba 1: " << number1 << endl;
     cout << "Liczba 2: " << number2 << endl;
 
     number1 = 63, number2 = 38;
+
     cout << endl << "Przed zamiana:" << endl;
     cout << "Liczba 1: " << number1 << endl;
     cout << "Liczba 2: " << number2 << endl;
 
     my_swap(&number1, &number2);
+
     cout << "Po zamianie:" << endl;
     cout << "Liczba 1: " << number1 << endl;
     cout << "Liczba 2: " << number2 << endl;
--- LAB2/Zadanie 2/zadanie_2.cpp
+++ LAB2/Zadanie 2/zadanie_2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void my_swap(int & number1, int & number2) {
+void my_swap(int &number1, int &number2) {
     int temp = number1;
     number1 = number2;
     number2 = temp;
@@ -10,11 +10,13 @@ void my_swap(int & number1, int & number2) {
 int main(int argc, char *argv[]) {
 
     int number1 = 17, number2 = 62;
+
     cout << "Przed zamiana:" << endl;
     cout << "Liczba 1: " << number1 << endl;
     cout << "Liczba 2: " << number2 << endl;
 
     my_swap(number1, number2);
+
     cout << "Po zamianie:" << endl;
     cout << "Liczba 1: " << number1 << endl;
     cout << "Liczba 2: " << number2 << endl;
