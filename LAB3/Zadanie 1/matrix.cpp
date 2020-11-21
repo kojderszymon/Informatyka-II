e9d7851d1ea912a882c182ef8eca2ef0488051ad
kojderszymon <vaser1999@gmail.com>
2020-11-21 19:02:37 +0100

    LAB3

--- LAB3/Zadanie 1/matrix.cpp
+++ LAB3/Zadanie 1/matrix.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include "matrix.hpp"
 
 Matrix::Matrix(int n) {
@@ -7,7 +8,7 @@ Matrix::Matrix(int n) {
     for(int i = 0; i < n; i++) {
         value[i] = new double [n];
         for(int j = 0; j < n; j++)
-            value[i][j] = 0,0;
+            value[i][j] = 0.0;
     }
 }
 
@@ -18,13 +19,13 @@ Matrix::Matrix(int n, int m) {
     for(int i = 0; i < n; i++) {
         value[i] = new double [m];
         for(int j = 0; j < m; j++)
-            value[i][j] = 0,0;
+            value[i][j] = 0.0;
     }
 }
 
-Matrix::Matrix(std::string filename) {
+//Matrix::Matrix(std::string filename) {
 
-}
+//}
 
 Matrix::~Matrix() {
     for(int i = 0; i < row; i++)
@@ -33,47 +34,79 @@ Matrix::~Matrix() {
 }
 
 void Matrix::set(int n, int m, double val) {
-    if((n < 0) || (n >= row) || (m < 0) || (m >= col))
+    if((n < 0) || (n >= row) || (m < 0) || (m >= col)) {
         std::cout << "Niepoprawne dane" << std::endl;
-    else
-        value[n][m] = val;
+        exit(1);
+    }
+    value[n][m] = val;
 }
 
 double Matrix::get(int n, int m) {
-    if((n < 0) || (n >= row) || (m < 0) || (m >= col))
+    if((n < 0) || (n >= row) || (m < 0) || (m >= col)) {
         std::cout << "Niepoprawne dane" << std::endl;
-    else
-        return value[n][m];
+        exit(2);
+    }
+    return value[n][m];   
 }
 
 Matrix Matrix::add(Matrix matrix_2) {
-
+    if((row != matrix_2.row) || (col != matrix_2.col)) {
+        std::cout << "Niepoprawne dane" << std::endl;
+        exit(3);
+    }
+    Matrix result(row, col);
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < col; j++)
+            result.value[i][j] = value[i][j] + matrix_2.value[i][j];
+    return result;
 }
 
 Matrix Matrix::substract(Matrix matrix_2) {
-
+    if((row != matrix_2.row) || (col != matrix_2.col)) {
+        std::cout << "Niepoprawne dane" << std::endl;
+        exit(3);
+    }
+    Matrix result(row, col);
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < col; j++)
+            result.value[i][j] = value[i][j] - matrix_2.value[i][j];
+    return result;
 }
 
 Matrix Matrix::multiply(Matrix matrix_2) {
-
+    row =row;
+    return matrix_2;
 }
 
 int Matrix::cols() {
-
+    return col;
 }
 
 int Matrix::rows() {
-
+    return row;
 }
 
 void Matrix::print() {
-
+    for(int i = 0; i < col; i++) {
+        for(int j = 0; j < row; j++)
+            std::cout << value[i][j] << " ";
+    std::cout << std::endl;
+    }
 }
 
 void Matrix::store(std::string filename, std::string path) {
-
+    std::fstream file;
+    file.open(filename, std::ios::out);
+    if(file.good() == true ) {
+        for(int i = 0; i < col; i++) {
+            for(int j = 0; j < row; j++)
+                file << value[i][j] << " ";
+            file << std::endl;
+        }
+        file.close();
+    }
 }
 
 int Matrix::main() {
-
+    return row;
 }
