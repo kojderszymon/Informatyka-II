f717f0aadabf361e0b5aa613fbb0dcd63b707ab3
kojderszymon <vaser1999@gmail.com>
2020-11-21 20:40:12 +0100

    LAB3

--- LAB3/Zadanie 1/main.cpp
+++ LAB3/Zadanie 1/main.cpp
@@ -1,9 +1,5 @@
 #include "matrix.cpp"
 
-int main(int argc, char *argv[]) {
-    Matrix m1(3);
-    m1.set(2, 1, 3.1);
-    std::cout << m1.get(1, 1) << std::endl;
-    m1.store("tekst.txt", "");
+main() {
     return 0;
 }
--- LAB3/Zadanie 1/matrix.cpp
+++ LAB3/Zadanie 1/matrix.cpp
@@ -24,9 +24,23 @@ Matrix::Matrix(int n, int m) {
 }
 
 Matrix::Matrix(std::string filename) {
-    row = 1;
-    col = 2;
-    //to
+    std::fstream file;
+    file.open(filename, std::ios::in);
+    if(file.good()) {
+        file >> row;
+        file >> col;
+        value = new double * [row];
+            for(int i = 0; i < row; i++) {
+                value[i] = new double [col];
+                for(int j = 0; j < col; j++)
+                    file >> value[i][j];
+            }
+        file.close();
+    }
+    else {
+        std::cout << "Tworzenie zmiennej nie powiodlo sie" << std::endl;
+        exit(1);
+    }
 }
 
 Matrix::~Matrix() {
@@ -38,7 +52,7 @@ Matrix::~Matrix() {
 void Matrix::set(int n, int m, double val) {
     if((n < 0) || (n >= row) || (m < 0) || (m >= col)) {
         std::cout << "Niepoprawne dane" << std::endl;
-        exit(1);
+        exit(2);
     }
     value[n][m] = val;
 }
@@ -46,7 +60,7 @@ void Matrix::set(int n, int m, double val) {
 double Matrix::get(int n, int m) {
     if((n < 0) || (n >= row) || (m < 0) || (m >= col)) {
         std::cout << "Niepoprawne dane" << std::endl;
-        exit(2);
+        exit(3);
     }
     return value[n][m];   
 }
@@ -54,7 +68,7 @@ double Matrix::get(int n, int m) {
 Matrix Matrix::add(Matrix matrix_2) {
     if((row != matrix_2.row) || (col != matrix_2.col)) {
         std::cout << "Niepoprawne dane" << std::endl;
-        exit(3);
+        exit(4);
     }
     Matrix result(row, col);
     for(int i = 0; i < row; i++)
@@ -66,7 +80,7 @@ Matrix Matrix::add(Matrix matrix_2) {
 Matrix Matrix::substract(Matrix matrix_2) {
     if((row != matrix_2.row) || (col != matrix_2.col)) {
         std::cout << "Niepoprawne dane" << std::endl;
-        exit(4);
+        exit(5);
     }
     Matrix result(row, col);
     for(int i = 0; i < row; i++)
@@ -78,7 +92,7 @@ Matrix Matrix::substract(Matrix matrix_2) {
 Matrix Matrix::multiply(Matrix matrix_2) {
     if(col != matrix_2.row) {
         std::cout << "Niepoprawne dane" << std::endl;
-        exit(5);
+        exit(6);
     }
     Matrix result(row, matrix_2.col);
     double sum;
@@ -110,11 +124,12 @@ void Matrix::print() {
 }
 
 void Matrix::store(std::string filename, std::string path) {
-    //to
+    char *fullpath;
+    sprintf(fullpath, "%s%s", path, filename);
     std::fstream file;
-    file.open(filename, std::ios::out);
-    if(file.good() == true ) {
-        file << row << "x" << col << std::endl;
+    file.open(fullpath, std::ios::out);
+    if(file.good()) {
+        file << row << " " << col << std::endl;
         for(int i = 0; i < col; i++) {
             for(int j = 0; j < row; j++)
                 file << value[i][j] << " ";
@@ -122,9 +137,41 @@ void Matrix::store(std::string filename, std::string path) {
         }
         file.close();
     }
+    else {
+        std::cout << "Zapis do pliku nie powiodl sie" << std::endl;
+        exit(7);
+    }
 }
 
 int Matrix::main() {
-    return row;
-    //to
+    std::cout << "Tworze macierz 3x3" << std::endl;
+    Matrix matrix_1(3);
+    matrix_1.print();
+    std::cout << "Tworze macierz 4x3" << std::endl;
+    Matrix matrix_2(4, 3);
+    matrix_2.print();
+    matrix_1.set(2, 2, 5);
+    std::cout << "Ustawiam wartosc w komorce 2x2 na " << matrix_1.get(2, 2) << std::endl;
+    matrix_1.print();
+    std::cout << "Tworze kolejna macierz 3x3" << std::endl;
+    Matrix matrix_3(3);
+    matrix_3.print();
+    std::cout << "Sume macierzy 1 i 3 zapisze do nowej zmiennej i pliku" << std::endl;
+    Matrix matrix_4(3);
+    matrix_4 = matrix_1.add(matrix_3);
+    matrix_4.print();
+    matrix_4.store("tekst.txt", "");
+    std::cout << "Roznice macierzy 1 i 3 zapisze do 4 zmiennej" << std::endl;
+    matrix_4 = matrix_1.substract(matrix_3);
+    matrix_4.print();
+    matrix_1.set(1, 1, 3);
+    std::cout << "Ustawiam wartosc w komorce 1x1 na " << matrix_1.get(1, 1) << std::endl;
+    matrix_1.print();
+    std::cout << "Iloczyn macierzy 1 i 2 zapisze do 4 zmiennej" << std::endl;
+    matrix_4 = matrix_1.multiply(matrix_2);
+    matrix_4.print();
+    std::cout << "Iloczyn ma " << matrix_4.rows() << " wierszy i " << matrix_4.cols() << " kolumn" << std::endl;
+    std::cout << "Do nowej zmiennej zapisze macierz zapisana w pliku" << std::endl;
+    Matrix matrix_5("tekst.txt");
+    return 0;
 }
