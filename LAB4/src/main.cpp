1c379dbc2b4a235da68b02108cd8d40a7ed0a906
kojderszymon <vaser1999@gmail.com>
2020-12-02 19:48:00 +0100

    LAB4

---
+++ LAB4/src/main.cpp
@@ -0,0 +1,44 @@
+#include <LAB4/matrix.hpp>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Tworze macierz 4x4 (matrix_1)" << std::endl;
+    Matrix matrix_1 = Matrix(4);
+    matrix_1.print();
+    std::cout << "Tworze macierz 4x5 (matrix_2)" << std::endl;
+    Matrix matrix_2 = Matrix(4, 5);
+    matrix_2.print();
+    matrix_1.set(2, 2, 5);
+    std::cout << "Ustawiam wartosc w komorce (2, 2) (matrix_1) na " << matrix_1.get(2, 2) << std::endl;
+    matrix_1.print();
+    std::cout << "Tworze kolejna macierz 4x4 (matrix_3)" << std::endl;
+    Matrix matrix_3 = Matrix(4);
+    matrix_3.print();
+    matrix_3.set(1, 1, 4);
+    std::cout << "Ustawiam wartosc w komorce (1, 1) (matrix_3) na " << matrix_3.get(1, 1) << std::endl;
+    matrix_3.print();
+    std::cout << "Sume macierzy 1 i 3 zapisze do nowej zmiennej (matrix_4) i pliku" << std::endl;
+    Matrix matrix_4 = matrix_1.add(matrix_3);
+    matrix_4.print();
+    matrix_4.store("tekst.txt", "./Files/");
+    std::cout << "Roznice macierzy 1 i 3 zapisze do zmiennej (matrix_5)" << std::endl;
+    Matrix matrix_5 = matrix_1.substract(matrix_3);
+    matrix_5.print();
+    std::cout << "Ustawiam rowne wartosci do macierzy 1 i 2" << std::endl;
+    matrix_1.set(0, 0, 1);
+    matrix_2.set(0, 0, 4);
+    matrix_1.set(2, 3, 5);
+    matrix_2.set(3, 2, 2);
+    matrix_2.set(2, 2, 3);
+    std::cout << "matrix_1" << std::endl;
+    matrix_1.print();
+    std::cout << "matrix_2" << std::endl;
+    matrix_2.print();
+    std::cout << "Iloczyn macierzy 1 i 2 zapisze do zmiennej nowej (matrix_6)" << std::endl;
+    Matrix matrix_6 = matrix_1.multiply(matrix_2);
+    matrix_6.print();
+    std::cout << "Iloczyn ma " << matrix_6.rows() << " wiersz/-e/-y i " << matrix_6.cols() << " kolumn/-e/-y" << std::endl;
+    std::cout << "Do nowej zmiennej zapisze macierz zapisana w pliku (matrix_7)" << std::endl;
+    Matrix matrix_7 = Matrix("./Files/tekst.txt");
+    matrix_7.print();
+    return 0;
+}
---
+++ LAB4/src/matrix.cpp
@@ -0,0 +1,138 @@
+#include <LAB4/matrix.hpp>
+
+Matrix::Matrix(int n) {
+    row = n;
+    col = n;
+    value = new double * [n];
+    for(int i = 0; i < n; i++) {
+        value[i] = new double [n];
+        for(int j = 0; j < n; j++)
+            value[i][j] = 0;
+    }
+}
+
+Matrix::Matrix(int n, int m) {
+    row = n;
+    col = m;
+    value = new double * [n];
+    for(int i = 0; i < n; i++) {
+        value[i] = new double [m];
+        for(int j = 0; j < m; j++)
+            value[i][j] = 0;
+    }
+}
+
+Matrix::Matrix(std::string filename) {
+    std::fstream file;
+    file.open(filename, std::ios::in);
+    if(file.good()) {
+        file >> row;
+        file >> col;
+        value = new double * [row];
+            for(int i = 0; i < row; i++) {
+                value[i] = new double [col];
+                for(int j = 0; j < col; j++)
+                    file >> value[i][j];
+            }
+        file.close();
+    }
+    else {
+        std::cout << "Tworzenie zmiennej nie powiodlo sie" << std::endl;
+        exit(1);
+    }
+}
+
+Matrix::~Matrix() {
+    for(int i = 0; i < row; i++)
+        delete [] value[i];
+    delete [] value;
+}
+
+bool Matrix::set(int n, int m, double val) {
+    if((n < 0) || (n >= row) || (m < 0) || (m >= col))
+        return false;
+    value[n][m] = val;
+    return true;
+}
+
+double Matrix::get(int n, int m) {
+    if((n < 0) || (n >= row) || (m < 0) || (m >= col)) {
+        std::cout << "Niepoprawne dane" << std::endl;
+        exit(2);
+    }
+    return value[n][m];   
+}
+
+Matrix Matrix::add(Matrix matrix_2) {
+    if((row != matrix_2.row) || (col != matrix_2.col)) {
+        std::cout << "Niepoprawne dane" << std::endl;
+        exit(3);
+    }
+    Matrix result = Matrix(row, col);
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < col; j++)
+            (result.value)[i][j] = value[i][j] + (matrix_2.value)[i][j];
+    return result;
+}
+
+Matrix Matrix::substract(Matrix matrix_2) {
+    if((row != matrix_2.row) || (col != matrix_2.col)) {
+        std::cout << "Niepoprawne dane" << std::endl;
+        exit(4);
+    }
+    Matrix result = Matrix(row, col);
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < col; j++)
+            (result.value)[i][j] = value[i][j] - (matrix_2.value)[i][j];
+    return result;
+}
+
+Matrix Matrix::multiply(Matrix matrix_2) {
+    if(col != matrix_2.row) {
+        std::cout << "Niepoprawne dane" << std::endl;
+        exit(5);
+    }
+    Matrix result = Matrix(row, matrix_2.col);
+    double sum;
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < matrix_2.col; j++) {
+            sum = 0;
+            for(int k = 0; k < col; k++)
+                sum += value[i][k] * (matrix_2.value)[k][j];
+            (result.value)[i][j] = sum;
+        }
+    return result;
+}
+
+int Matrix::cols() {
+    return col;
+}
+
+int Matrix::rows() {
+    return row;
+}
+
+void Matrix::print() {
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++)
+            std::cout << value[i][j] << " ";
+    std::cout << std::endl;
+    }
+}
+
+bool Matrix::store(std::string filename, std::string path) {
+    std::fstream file;
+    file.open((path + filename).c_str(), std::ios::out);
+    if(file.good()) {
+        file << row << " " << col << std::endl;
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++)
+                file << value[i][j] << " ";
+            file << std::endl;
+        }
+        file.close();
+        return true;
+    }
+    else
+        return false;
+}
